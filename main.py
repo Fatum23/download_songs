ebd2b4d4b6f1fbdc7c9877d2209d3915651d7bbc
Fatum23 <115827692+Fatum23@users.noreply.github.com>
2024-02-24 18:10:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pytube import Playlist, YouTube
-import youtube_dl
 import re
+import os
 
 URl = "https://www.youtube.com/playlist?list=PLLPYwPfN7fKQxDncTGmjP2jhqslGLsTQR"
 
@@ -8,16 +8,32 @@ URl = "https://www.youtube.com/playlist?list=PLLPYwPfN7fKQxDncTGmjP2jhqslGLsTQR"
 playlist = Playlist(URl)
 
 
-for video in playlist.videos:
-    video_title = video.title
-    video_url = video.watch_url
-    print(f"Название: {video_title}")
-    print(f"URL: {video_url}\n")
+for count, video in enumerate(playlist.videos):
+    tries = 0
+    while True:
+        if tries > 5:
+            print("Too many attempts, skipping...")
+            break
+        try:
+            video_title = video.title
+            video_url = video.watch_url
+            print(f"Название: {video_title}")
+            print(f"URL: {video_url}\n")
+        
+            yt = YouTube(video_url)
+            audio = yt.streams.get_audio_only()  # Get the audio stream
+            audio_title = yt.title
+            audio_title = re.sub(r'\W+', ' ', audio_title)
+            audio_filename = f"results/{audio_title}.mp3"  # Specify the filename to save the audio
+            if not os.path.isfile(audio_filename):
+                audio.download(filename=audio_filename)
+                print(f"Download complete: {audio_filename}")
+            else:
+                print("Already downloaded")
+            print(f"{count + 1} / {len(playlist.videos)}")
+            tries = 0
+            break
+        except Exception as e:
+            tries += 1
+            print(e)￼Enter file contents here
 
-    yt = YouTube(video_url)
-    audio = yt.streams.get_audio_only()  # Get the audio stream
-    audio_title = yt.title
-    audio_title = re.sub(r'\W+', ' ', audio_title)
-    audio_filename = f"results/{audio_title}.mp3"  # Specify the filename to save the audio
-    audio.download(filename=audio_filename)
-    print(f"Download complete: {audio_filename}")
