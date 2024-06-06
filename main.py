f7debedd1bd12f61094fe43400ee2baf73df56eb
Fatum23 <115827692+Fatum23@users.noreply.github.com>
2024-06-06 12:39:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from pytube import Playlist, YouTube
+from pytube.innertube import _default_clients
+
+_default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID_CREATOR"]
 import re
 import os
 
@@ -21,19 +24,21 @@ for count, video in enumerate(playlist.videos):
             print(f"URL: {video_url}\n")
         
             yt = YouTube(video_url)
-            audio = yt.streams.get_audio_only()  # Get the audio stream
-            audio_title = yt.title
-            audio_title = re.sub(r'\W+', ' ', audio_title)
-            audio_filename = f"results/{audio_title}.mp3"  # Specify the filename to save the audio
-            if not os.path.isfile(audio_filename):
-                audio.download(filename=audio_filename)
-                print(f"Download complete: {audio_filename}")
+            if yt.length > 60 * 60 * 3:
+                print("Too long video")
             else:
-                print("Already downloaded")
+                audio = yt.streams.get_audio_only()  # Get the audio stream
+                audio_title = yt.title
+                audio_title = re.sub(r'\W+', ' ', audio_title)
+                audio_filename = f"results/{audio_title}.mp3"  # Specify the filename to save the audio
+                if not os.path.isfile(audio_filename):
+                    audio.download(filename=audio_filename)
+                    print(f"Download complete: {audio_filename}")
+                else:
+                    print("Already downloaded")
             print(f"{count + 1} / {len(playlist.videos)}")
             tries = 0
             break
         except Exception as e:
             tries += 1
-            print(e)￼Enter file contents here
-
+            print(e)
